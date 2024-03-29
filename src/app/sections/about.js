9f08b764e2873b1cf5097fcaf99935254de3a7dc
jurecapuder <capuder.j@gmail.com>
2024-03-29 19:55:58 +0100

    Update About section with new content

--- src/app/sections/about.js
+++ src/app/sections/about.js
@@ -2,8 +2,22 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div className="flex justify-between">
+      <img src="/roke.png" alt="Roke" className="w-1/2" />
+      <div className="w-1/2">
+        <h2 className="text-2xl font-bold mb-4">
+          Fist. Neomejene možnosti s plastičnimi granulati.
+        </h2>
+
+        <p>
+          Ukvarjamo se z distribucijo plastičnih granulatov sit amet consectetur.
+          Aliquet mauris amet morbi tincidunt orci vitae commodo.
+          Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam. 
+          Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam. 
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default About;

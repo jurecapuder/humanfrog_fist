3371c67dcc7bbc400dd6e9d7f67566bcc1419a9b
jurecapuder <capuder.j@gmail.com>
2024-03-29 15:02:32 +0100

    Add SatisfiedCustomers component

---
+++ src/app/sections/satisfiedCustomers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SatisfiedCustomers() {
+  return (
+    <div>
+      <h2>Satisfied Customers</h2>
+      <p>Our customers love our products!</p>
+    </div>
+  );
+}
+
+export default SatisfiedCustomers;

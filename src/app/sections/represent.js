5bdc1a839fb03b26ed2ff02f9bf11e7b42d42086
jurecapuder <capuder.j@gmail.com>
2024-03-29 19:35:41 +0100

    Add image slider functionality to Represent component

--- src/app/sections/represent.js
+++ src/app/sections/represent.js
@@ -1,11 +1,36 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+
+const images = ["/borealis.png", "/synthos.png", "/chemtrend.png", "/viba.png"]; // Add your images here
 
 const Represent = () => {
-  return (
-    <div>
+  const [current, setCurrent] = useState(0);
 
+  function goLeft() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
+  function goRight() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  return (
+    <div className="text-center">
+      <h2>NAŠE ZADOVOLJNE STRANKE</h2>
+      <div className="border-t border-fist-oranzna h-px w-32 mx-auto mt-5" />
+
+      <div className="slider">
+        {images.map((img, idx) => (
+          <div className={idx === current ? "slide active" : "slide"} key={idx}>
+            {idx === current && (<img src={img} alt="Customer" />)}
+          </div>
+        ))}
+        <button onClick={goLeft}>Go Left</button>
+        <button onClick={goRight}>Go Right</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Represent;

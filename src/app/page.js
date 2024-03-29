4f0257cd082c85fa1073398fe510294e5ddfce20
jurecapuder <capuder.j@gmail.com>
2024-03-29 15:03:49 +0100

    Refactor page.js: Add Header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,14 @@
 import React from "react";
-import Image from "next/image";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import Header from "./sections/header";
 import SatisfiedCustomers from "./sections/satisfiedCustomers";
 import Footer from "./sections/footer";
 
 export default function Home() {
   return (
     <div className="bg-white text-logo-blue">
-      <header className="flex items-center justify-between" style={{ padding: '0 10vw' }}>
-        <Image src="/fist.png" alt="Company Logo" width={167} height={79} />
-        
-        <nav>
-          <ul className="flex space-x-10">
-            <li><a href="#home"><i className="fas fa-home"></i></a></li> {/* House icon */}
-            <li><a href="#prodajni-program">Prodajni program</a></li>
-            <li><a href="#zastopamo">Zastopamo</a></li>
-            <li><a href="#svet-360">Svet 360&#176;</a></li>
-            <li><a href="#baza-znanja">Baza znanja</a></li>
-            <li><a href="#o-nas">O nas</a></li>
-          </ul>
-        </nav>
-
-        <button className="bg-purple-500 text-white rounded-full px-4 py-2">Povpraševanje</button>
-        
-        <div className="relative inline-flex">
-          <Image className="inline-block" src="/slovenian-flag.png" alt="Slovenia Flag" width={32} height={18} />
-          <select className="ml-2">
-            {/* Add more language options here */}
-          </select>
-        </div>
-      </header>
+      <Header />
       
       <section className="relative bg-cover bg-center" style={{ backgroundImage: `url(/background-image.jpg)` }}>
         <div className="w-full h-20 bg-blue-800 opacity-90 flex items-center justify-end mr-28">

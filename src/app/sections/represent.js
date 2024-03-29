e0792fa46d21c623a88103cbc555148a17705a39
jurecapuder <capuder.j@gmail.com>
2024-03-29 19:50:08 +0100

    Add image slideshow functionality

--- src/app/sections/represent.js
+++ src/app/sections/represent.js
@@ -1,20 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const images = ["/borealis.png", "/synthos.png", "/chemtrend.png", "/viba.png"]; // Add your images here
 
 const Represent = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  const getImages = () => {
+    let imgArray = [];
+    for (let i = 0; i < 4; i++) {
+      imgArray.push(images[(currentIndex + i) % images.length]);
+    }
+    return imgArray;
+  };
+
   return (
     <div className="text-center">
       <h2>ZASTOPAMO</h2>
       <div className="border-t border-fist-oranzna h-px w-32 mx-auto mt-5" />
 
       <div className="flex justify-around mt-10">
-        {images.map((img, idx) => (
+        {getImages().map((img, idx) => (
           <div className="slide" key={idx}>
             <img src={img} alt="Customer" />
           </div>
         ))}
       </div>
+
+      <button onClick={handlePrev}>Prev</button>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 };

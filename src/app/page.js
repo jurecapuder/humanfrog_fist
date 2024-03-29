2b814ac4e1ca0be4fcd5326ce331fa500a816051
jurecapuder <capuder.j@gmail.com>
2024-03-29 15:05:49 +0100

    Add Banner component to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Header from "./sections/header";
+import Banner from "./sections/banner";
 import SatisfiedCustomers from "./sections/satisfiedCustomers";
 import Footer from "./sections/footer";
 
@@ -10,28 +11,7 @@ export default function Home() {
     <div className="bg-white text-logo-blue">
       <Header />
       
-      <section className="relative bg-cover bg-center" style={{ backgroundImage: `url(/background-image.jpg)` }}>
-        <div className="w-full h-20 bg-blue-800 opacity-90 flex items-center justify-end mr-28">
-          <div className="flex items-center border-2 border-gray-300 p-2 rounded-md">
-            <input className="bg-transparent outline-none flex-grow w-80" type="text" placeholder="Iskanje po prodajnem programu" />
-            <i className="fas fa-search"></i>
-          </div>
-        </div>
-
-        <div className="px-28 w-1/3 relative bg-blue-800 bg-opacity-90 pl-4 text-white flex flex-col items-center">
-          <h2 className="text-4xl font-bold">Naši plastični granulati</h2>
-          <h2 className="text-4xl font-bold">Vaši trajnostno<br />naravni izdelki</h2>
-          <div className="w-1/2 h-1 bg-orange-500 my-4"></div>
-          <p className="text-lg">Ukvarjamo se z distribucijo plastičnih granulatov sit amet consectetur. Aliquet mauris amet morbi tincideunt ori c vitae commodo.</p>
-          <div className="flex mt-4">
-            <button className="bg-blue-800 text-white rounded-full px-4 py-2 mr-4">Prodajni program</button>
-            <button className="border-2 border-blue-800 text-blue-800 rounded-full px-4 py-2">Povpraševanje</button>
-          </div>
-          <div className="mt-8">
-            <i className="fas fa-arrow-down"></i>
-          </div>
-        </div>
-      </section>
+      <Banner />
       
       <section>
         <div className="flex flex-col items-center">

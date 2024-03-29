aa05876563687f67cf59564a3eaf9fae6cbf4050
jurecapuder <capuder.j@gmail.com>
2024-03-29 20:03:08 +0100

    Refactor about section markup

--- src/app/sections/about.js
+++ src/app/sections/about.js
@@ -5,16 +5,16 @@ const About = () => {
     <div className="flex justify-between">
       <img src="/roke.png" alt="Roke" className="w-1/2" />
       <div className="w-1/2">
-        <h2 className="text-2xl font-bold mb-4">
-          Fist. Neomejene možnosti s plastičnimi granulati.
+        <h2 className="text-2xl mb-4">
+          <strong>Fist.</strong> Neomejene možnosti s plastičnimi granulati.
         </h2>
 
-        <p>
-          Ukvarjamo se z distribucijo plastičnih granulatov sit amet consectetur.
-          Aliquet mauris amet morbi tincidunt orci vitae commodo.
-          Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam. 
-          Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam. 
-        </p>
+        <ul className="list-disc list-inside">
+          <li>Ukvarjamo se z distribucijo plastičnih granulatov sit amet consectetur.</li>
+          <li>Aliquet mauris amet morbi tincidunt orci vitae commodo.</li>
+          <li>Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam.</li>
+          <li>Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam.</li>
+        </ul>
       </div>
     </div>
   );

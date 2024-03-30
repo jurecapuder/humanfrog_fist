f2288382f5c1ffaeea588c5359dd4dc77848a1f1
jurecapuder <capuder.j@gmail.com>
2024-03-31 00:40:58 +0100

    Refactor about section layout

--- src/app/sections/about.js
+++ src/app/sections/about.js
@@ -3,20 +3,22 @@ import React from "react";
 const About = () => {
   return (
     <div className="flex justify-between">
-      <div style={{ width: '40%', marginLeft: '10%' }}>
-        <img src="/roke.png" alt="Roke" className="w-full h-auto object-contain" style={{ backgroundColor: 'transparent' }} />
+      <div style={{ width: '40%', marginLeft: '10%' }} className="mr-5">
+        <img src="/roke.png" alt="Roke" className="w-full h-auto object-contain" />
       </div>
-      <div style={{ width: '40%', marginRight: '10%' }}>
-        <h2 className="text-2xl mb-4">
-          <strong>Fist.</strong> Neomejene možnosti s plastičnimi granulati.
-        </h2>
+      <div style={{ width: '40%', marginRight: '10%' }} className="ml-5 flex items-center text-left">
+        <div>
+          <h2 className="text-2xl mb-4">
+            <strong>Fist.</strong> Neomejene možnosti s plastičnimi granulati.
+          </h2>
 
-        <ul className="list-disc list-inside">
-          <li>Ukvarjamo se z distribucijo plastičnih granulatov sit amet consectetur.</li>
-          <li>Aliquet mauris amet morbi tincidunt orci vitae commodo.</li>
-          <li>Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam.</li>
-          <li>Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam.</li>
-        </ul>
+          <ul className="list-disc list-inside">
+            <li>Ukvarjamo se z distribucijo plastičnih granulatov sit amet consectetur.</li>
+            <li>Aliquet mauris amet morbi tincidunt orci vitae commodo.</li>
+            <li>Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam.</li>
+            <li>Eget tempor cras curabitur nec pellentesque scelerisque et in. Id turpis dictum egestas id enim at aliquam.</li>
+          </ul>
+        </div>
       </div>
     </div>
   );

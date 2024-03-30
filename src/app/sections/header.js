7567299b0bbe7477cc427ca609dd61fefe343440
jurecapuder <capuder.j@gmail.com>
2024-03-30 16:51:52 +0100

    Update home icon in header

--- src/app/sections/header.js
+++ src/app/sections/header.js
@@ -8,7 +8,13 @@ function Header() {
         
         <nav>
           <ul className="flex space-x-10">
-            <li><a href="#home"><i className="fas fa-home"></i></a></li> {/* House icon */}
+            <li>
+              <a href="#home">
+                <svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M27.8906 14.1211L26.1406 15.9531L25.293 15.1328V28.75H2.48828V15.3516L1.88672 15.9531L0.109375 14.0938L14 0.832031L27.8906 14.1211ZM22.75 26.207V12.6992L14 4.33203L5.05859 12.918V26.207H22.75Z" fill="#434A8D"/>
+                </svg>  
+              </a>
+            </li>
             <li><a href="#prodajni-program">Prodajni program</a></li>
             <li><a href="#zastopamo">Zastopamo</a></li>
             <li><a href="#svet-360">Svet 360&#176;</a></li>

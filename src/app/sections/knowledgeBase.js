0c0e5ccdbb85ca12a5d02e4df3e41dbda327586f
jurecapuder <capuder.j@gmail.com>
2024-03-31 14:21:11 +0200

    Refactor knowledgeBase.js component styling

--- src/app/sections/knowledgeBase.js
+++ src/app/sections/knowledgeBase.js
@@ -15,17 +15,18 @@ const KnowledgeBase = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
   return (
-    <div className="knowledge-base flex">
-      <div className="left-column w-1/3">
+    <div className="flex bg-[#151A45] text-white mt-10 mb-10 pl-10 pr-10">
+      <div className="w-1/2">
         <h2 className="text-center">Baza znanja</h2>
         <div className="border-t border-fist-oranzna h-px w-32 mx-auto mt-5" />
       </div>
-      <div className="right-column w-2/3">
+      <div className="w-1/2">
         {questions.map((item, index) => (
-          <div key={index}>
+          <div key={index} className="mb-4">
             <div
               className={`question cursor-pointer ${openIndex === index ? "text-fist-oranzna" : ""}`}
-              onClick={() => setOpenIndex(index)}
+              onClick={() => setOpenIndex(openIndex === index ? null : index)}
+              style={{ lineHeight: "1.5" }}
             >
               {item.question}
               <span className="arrow float-right">
@@ -33,7 +34,7 @@ const KnowledgeBase = () => {
               </span>
             </div>
             {openIndex === index && (
-              <div className="text-black">{item.answer}</div>
+              <div className="text-black" style={{ lineHeight: "1.5" }}>{item.answer}</div>
             )}
             <hr className="separator border-t border-black" />
           </div>

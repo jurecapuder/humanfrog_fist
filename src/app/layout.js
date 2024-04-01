8e48611d459fd016a73c02eee2323772cc2be0b0
jurecapuder <capuder.j@gmail.com>
2024-04-01 15:50:47 +0200

    Add header and footer components to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
+import Header from "./sections/header";
+import Footer from "./sections/footer";
+
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
   style: ['normal', 'italic'],
@@ -16,7 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${roboto.className} bg-[#EBEDEE] text-logo-blue`}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
